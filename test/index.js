1796dadb2d3d4e959dec307b73df0874606daf28
Attila Beregszaszi <attila@frontseed.com>
2018-04-22 11:08:08 +0100

    test: Upgrade Mocha and simplify tests

--- test/index.js
+++ test/index.js
@@ -6,24 +6,21 @@ const Metalsmith = require('metalsmith')
 const rollup = require('../lib/')
 
 describe('metalsmith-rollup', () => {
-  it('rolls up basic', done => {
+  it('rolls up basic', () => {
     (new Metalsmith('test/fixtures/basic'))
       .use(rollup({
         entry: path.resolve(__dirname, 'fixtures/basic/src/main.js'),
         format: 'iife',
         dest: 'bundle.js'
       }))
-      .build(function (err, files) {
-        if (err) {
-          return done(err)
-        }
+      .build((err, files) => {
+        if (err) throw err
         assert.equal(Object.keys(files).length, 4)
         assertDir('test/fixtures/basic/expected', 'test/fixtures/basic/build')
-        done()
       })
   })
 
-  it('rolls up with source map', done => {
+  it('rolls up with source map', () => {
     (new Metalsmith('test/fixtures/sourcemap'))
       .use(rollup({
         entry: path.resolve(__dirname, 'fixtures/sourcemap/src/main.js'),
@@ -33,29 +30,22 @@ describe('metalsmith-rollup', () => {
       }, {
         ignoreSources: true
       }))
-      .build(function (err, files) {
-        if (err) {
-          return done(err)
-        }
+      .build((err, files) => {
+        if (err) throw err
         assert.equal(Object.keys(files).length, 3)
         assertDir('test/fixtures/sourcemap/expected', 'test/fixtures/sourcemap/build')
-        done()
       })
   })
 
-  it('reports errors', done => {
+  it('reports errors', () => {
     (new Metalsmith('test/fixtures/basic'))
       .use(rollup({
         entry: path.resolve(__dirname, 'fixtures/faulty/src/main.js'),
         format: 'iife',
         dest: 'bundle.js'
       }))
-      .build(function (err) {
-        if (err) {
-          assert.equal(err.message.match(/Could not resolve \.\/mathz/).length, 1)
-          done()
-        }
-        throw new Error('Error excepted, none occurred!')
+      .build(err => {
+        assert(err.message.includes('Could not resolve ./mathz'))
       })
   })
 })
